dc19b8c9caf55b2d4fc8e4901280127ab91c0494
Bartosz Bujakowski <bbujakowski@efigence.com>
2017-08-11 14:18:47 +0200

    Adds review comments

--- js/validate.js
+++ js/validate.js
@@ -5,12 +5,17 @@ document.addEventListener('DOMContentLoaded', function () {
         const input = document.querySelector('#input-password');
 
         button.addEventListener("click", function(event) {
+			// unikamy zostawiania console.logów w kodzie
             console.log(input.value);
 
+			// skoro używasz const'ów to polecem tutaj użyc 'let' a o czymś takim jak
+			// var zapomniec, że istnieje =) Pozatym alert nie jest najlepszą nazwą
+			// bo pokrywa się z natywną funkcją alert()
             var alert = document.querySelector('#password-alert');
 
             if (input.value.length < 8) {
-        		alert.innerHTML = "Password is too short. It has to be at least 8 characters long!";
+				alert.innerHTML = "Password is too short. It has to be at least 8 characters long!";
+				// spyrytne, ale kilkukrotne wywołanie funkcji sprawi, że będziemy miec wiele powtórzeń error'ów
         		input.className += ' error';
 	        }
 	        if (input.value === '') {
@@ -20,7 +25,8 @@ document.addEventListener('DOMContentLoaded', function () {
 	        	console.log(window.location);
 	        	window.location.href = 'index.html';
 	        }
-        });
+		});
+		// zbędne entery =)
 
 
 
